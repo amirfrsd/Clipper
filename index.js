6ce7a96f73e5a687ca6a4225527e20e79e9cff31
Amir Hossein <amir@Amirs-iMac-2.local>
2017-07-09 04:53:21 +0430

    First commit

---
+++ index.js
@@ -0,0 +1,38 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var morgan = require('morgan');
+const fileUpload = require('express-fileupload');
+
+var app = express();
+var port = process.env.port || 8191;
+var router = express.Router();
+
+app.use(morgan('dev'));
+app.use(fileUpload());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use('/', router);
+
+function guid() {
+  function s4() {
+    return Math.floor((1 + Math.random()) * 0x10000)
+      .toString(16)
+      .substring(1);
+  }
+  return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
+    s4() + '-' + s4() + s4() + s4();
+}
+
+app.post('/upload', function(req, res){
+    if (!req.files)
+        return res.status(400).json({success:false,message:'No files are uploaded.'});
+    let file = req.files.picture;
+    let uuid = guid();
+    file.mv('../static_content/' + uuid + '.jpg', function(err){
+        if (err)
+            return res.status(500).json({success:false, message:err});
+        res.json({success:true, message:'https://clipper.myfuckingapi.com/'+ uuid + '.jpg'})
+    })
+});
+
+app.listen(port);
